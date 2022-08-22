212e86e8237094b02a23bcdf33fe154fa5d58fef
Nelquin Budde <budde936@gmail.com>
2022-08-22 10:16:30 -0600

    deploying try 4

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Library App</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 </head>
 
 <body>
     <div id="app"></div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="/js/app.js"></script>
 </body>
 
 </html>
